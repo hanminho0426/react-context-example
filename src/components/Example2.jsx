8703ab15ea4febec22b49fc21c3d91edfd324236
Mino_Fe <86613190+hanminho0426@users.noreply.github.com>
2021-09-21 15:58:47 +0900

    생성

---
+++ src/components/Example2.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PersonContext from "../contexts/PersonContext";
+
+export default class Example2 extends React.Component {
+  static contextType = PersonContext;
+  render() {
+    const persons = this.context;
+    return (
+      <ul>
+        {" "}
+        <ul>
+          {persons.map((person) => (
+            <li>{person.name}</li>
+          ))}
+        </ul>
+      </ul>
+    );
+  }
+}
+
+Example2.contextType = PersonContext;
